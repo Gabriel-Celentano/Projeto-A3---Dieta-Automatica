418cda2a7eaf79a35124eba5e8f9646a91d45ca7
fernando-fukunaga <fernandofukunaga14@gmail.com>
2024-09-29 21:28:43 -0300

    feat: adicionado os dois primeiros componentes

--- dietautomatica/src/index.js
+++ dietautomatica/src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Cabecalho = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-center align-items-center">
+        <a className="navbar-brand" href="#" style={{ fontSize: '2rem' }}>
+          <i class="fa-solid fa-bowl-food"></i>
+          &nbsp;Dieta Automática
+        </a>
+      </div>
+    </nav>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const FormPrincipal = () => {
+  const subirAlert = (event) => {
+    event.preventDefault()
+    alert("Enviado!")
+  }
+
+  return (
+    <div className='container' style={{ marginTop: 30 }}>
+      <h3 className='text-center'>Formulário para Geração de Dieta</h3>
+      <form onSubmit={subirAlert}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">Liste os alimentos que você comprou para a semana (se possível, com quantidades, peso)</label>
+          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea2" className="form-label">Liste alimentos que você não gosta, é alérgico, possui restrição, etc.</label>
+          <textarea className="form-control" id="exampleFormControlTextarea2" rows="3"></textarea>
+        </div>
+        <label className='form-label'>Selecione o seu objetivo</label>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Escolher</option>
+          <option value="1">Emagrecimento/Défcit Calórico</option>
+          <option value="2">Ganho de Massa Muscular</option>
+          <option value="3">Energia (para competições de corrida, maratonas, etc.)</option>
+          <option value="4">Aumento da Imunidade</option>
+        </select> 
+        <div className="col-12 mt-3">
+          <button class="btn btn-primary" type="submit">Gerar Dieta</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Cabecalho />
+      <FormPrincipal />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
